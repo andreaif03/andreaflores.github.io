2e03917694fac32c68e79b18346cd85420351272
Andrea I. Flores Piñeyro <163619575+andreaif03@users.noreply.github.com>
2024-03-17 22:21:35 -0600

    Update Variables 2.js

--- ACTIVIDAD_7/Variables/Variables 2.js
+++ ACTIVIDAD_7/Variables/Variables 2.js
@@ -3,7 +3,7 @@
 let myName = 'Andrea'; //Corrige el valor almacenado en la variable myName existente y asigna mi nombre a la variable myName
 
 //Don't edit the code below here!
-section.innerHTML = ' '; //Editar el código, borra el contenido HTML dentro del elemento con id "section"
+const section = document.querySelector('section'); //Editar el código, borra el contenido HTML dentro del elemento con id "section"
 
 const para = document.createElement('p'); //Crea un nuevo elemento de párrafo (<p>)
 para.textContent = myName; //Establece su contenido de texto como el valor de myName
